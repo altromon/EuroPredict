68def5ca262dfce8a90a7caf8c3ca4b3c4557596
Reypotxo <reypotxo@hotmail.com>
2019-04-24 22:10:41 +0200

    Added Excel repository

---
+++ EuroPredict/EuroPredict.Core.ExcelCombinationRepository/Combination.cs
@@ -0,0 +1,12 @@
+﻿using EuroPredict.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace EuroPredict.Core.OnlineExcelCombinationRepository
+{
+    public class Combination : ICombination
+    {
+        public IEnumerable<int> Columns { get; set; }
+        public IEnumerable<int> Stars { get; set; }
+    }
+}
---
+++ EuroPredict/EuroPredict.Core.ExcelCombinationRepository/CombinationRepository.cs
@@ -0,0 +1,17 @@
+﻿using EuroPredict.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EuroPredict.Core.OnlineExcelCombinationRepository
+{
+    public class CombinationRepository : ICombinationRepository
+    {
+        public string DataSource { get; set; }
+
+        public IEnumerable<ICombination> GetCombinations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
