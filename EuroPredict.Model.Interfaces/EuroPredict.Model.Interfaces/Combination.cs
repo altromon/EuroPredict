4d8b3279fd5aade13f5eaeca936bc66cfc5825f2
Reypotxo <reypotxo@hotmail.com>
2019-04-24 21:44:55 +0200

    Solution renamed Added model

---
+++ EuroPredict.Model.Interfaces/EuroPredict.Model.Interfaces/Combination.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EuroPredict.Model.Interfaces
+{
+    interface Combination
+    {
+        IEnumerable<int> Columns { get; set; }
+        IEnumerable<int> Stars { get; set; }
+
+    }
+}
---
+++ EuroPredict.Model.Interfaces/EuroPredict.Model.Interfaces/NumberInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EuroPredict.Model.Interfaces
+{
+    interface NumberInfo
+    {
+        int Number { get; set; }
+        int TotalTimes { get; set; }
+    }
+}
